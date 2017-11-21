16af8920249b19827353d84fa3a23d7816660cf6
Fued Alejandro Majul Ramirez <Fued2@live.com.mx>
2017-11-21 15:26:06 -0600

    Add files via upload

---
+++ Page1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xapp
+{
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+			InitializeComponent ();
+        }
+        async void OnbtnpageClickme(object sender, EventArgs arg)
+        {
+            Button Button = (Button)sender;
+            await DisplayAlert("Loged",
+        "Las credenciales se introdujeron correctamente", "Siguiente");
+            await Navigation.PushModalAsync(new Page2());
+        }
+
+    }
+}
+;
